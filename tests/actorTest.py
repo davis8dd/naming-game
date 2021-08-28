9ead0ad1a97da6f2c0be8be57d74429f29429d43
davis8dd <davis8dd@gmail.com>
2021-08-28 13:40:31 -0700

    Fix actor tests

--- tests/actorTest.py
+++ tests/actorTest.py
@@ -11,52 +11,26 @@ class TestNamingGame(unittest.TestCase):
         actor2 = Actor("Actor2", ["one", "two"])
         self.assertTrue(actor2.vocabulary == ["one", "two"])
 
-    """
-    def test_speak_single_word_to_listener(self):
-        speaker = Actor(["asdf"])
-        listener = Actor()
-
-        print("Speaker vocab is " + str(speaker.getVocabulary()))
-        speaker.speakTo(listener=listener, wordToSpeak = "hi")
-        print("Listener vocab is " + str(listener.getVocabulary()))
-
-        self.assertTrue(speaker.getVocabulary() == listener.getVocabulary())
-    """
-
     def test_naming_game_failure(self):
         speakerVocab = ["ATSALLAD", "AKNORAB", "AVLA"]
         listenerVocab = ["TARRAB", "AVLA", "OTEROL"]
         listenerVocabAfter = ["TARRAB", "AVLA", "OTEROL", "ATSALLAD"]
 
-        print("Testing failed naming game iteration")
-        speaker = Actor(speakerVocab)
-        listener = Actor(listenerVocab)
-        print("Before speaking:")
-        print("Speaker vocab is " + str(speaker.getVocabulary()))
-        print("Listener vocab is " + str(listener.getVocabulary()))
+        speaker = Actor("speaker", speakerVocab)
+        listener = Actor("listener", listenerVocab)
         speaker.speakTo(listener=listener, wordToSpeak="ATSALLAD")
-        print("After speaking:")
-        print("Speaker vocab is " + str(speaker.getVocabulary()))
-        print("Listener vocab is " + str(listener.getVocabulary()))
-        self.assertTrue(listener.getVocabulary() == listenerVocabAfter)
+        self.assertEqual(listener.getVocabulary(), listenerVocabAfter)
 
     def test_naming_game_success(self):
         speakerVocab = ["ATSALLAD", "AKNORAB", "AVLA"]
         listenerVocab = ["TARRAB", "AVLA", "OTEROL"]
         vocabAfter = ["AVLA"]
 
-        print("Testing successful naming game iteration")
-        speaker = Actor(speakerVocab)
-        listener = Actor(listenerVocab)
-        print("Before speaking:")
-        print("Speaker vocab is " + str(speaker.getVocabulary()))
-        print("Listener vocab is " + str(listener.getVocabulary()))
+        speaker = Actor("speaker", speakerVocab)
+        listener = Actor("listener", listenerVocab)
         speaker.speakTo(listener=listener, wordToSpeak="AVLA")
-        print("After speaking:")
-        print("Speaker vocab is " + str(speaker.getVocabulary()))
-        print("Listener vocab is " + str(listener.getVocabulary()))
-        self.assertTrue(listener.getVocabulary() == vocabAfter)
-        self.assertTrue(speaker.getVocabulary() == vocabAfter)
+        self.assertEqual(listener.getVocabulary(), vocabAfter)
+        self.assertEqual(speaker.getVocabulary(), vocabAfter)
 
     def test_get_vocabulary_size(self):
         speakerVocab = ["ATSALLAD", "AKNORAB", "AVLA"]

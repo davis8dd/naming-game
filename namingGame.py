616ae76b9f8ee6c46e2187fe309275dde3269acd
davis8dd <davis8dd@gmail.com>
2021-08-28 12:53:35 -0700

    Update stats file output

--- namingGame.py
+++ namingGame.py
@@ -2,29 +2,31 @@ from actor import Actor
 
 import logging
 import random
+import time
 
 LOGGER = logging.getLogger('NamingGame')
 LOGGER.level = logging.INFO
 
 class NamingGame(object):
-    def __init__(self):
+    def __init__(self, maxIterations = 10):
+        self.maxIterations = maxIterations
         self.actors = list()
         self.newWords = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
         LOGGER.info("Starting NamingGame")
 
     def __repr__(self):
-        return "The internal representation of a NamingGame"
+        return "NamingGame(actors=" + str(self.actors) + ", maxIterations=" + str(self.maxIterations) + ")"
 
     def __str__(self):
         return "A naming game instance with " + str(self.actors) + " actors."
 
-    def play(self, maxIterationsToPlay):
+    def play(self):
         LOGGER.info("Starting naming game with " + str(self.actors)
-                + " fully connected actors, and will play for " + str(maxIterationsToPlay) + " iterations.")
+                + " fully connected actors, and will play for " + str(self.maxIterations) + " iterations.")
         iteration = 0
         numberOfWords = 3
-        statsString = "[[numberOfWords = " + str(numberOfWords) + ", iterations = " + str(maxIterationsToPlay) + "],\n"
-        while iteration < maxIterationsToPlay and numberOfWords > 1:
+        statsString = '{"run-' + str(time.time()) + '": [[numberOfWords = ' + str(numberOfWords) + ", iterations = " + str(self.maxIterations) + "],\n"
+        while iteration < (self.maxIterations + 1) and numberOfWords > 1:
             LOGGER.info("Starting iteration " + str(iteration))
             statsString = statsString + "[" + str(iteration) + ", " + str(self.getNumberOfWords()) + ", " + str(self.getNumberOfUniqueWords()) + "],\n"
             self.iterate()
@@ -32,7 +34,7 @@ class NamingGame(object):
             LOGGER.info("After iteration " + str(iteration) + ", the game state is:")
             for actor in self.actors:
                 LOGGER.info("    " + str(actor))
-        statsString = statsString + "]"
+        statsString = statsString + "]}"
         writeToFile("stats.log", statsString)
 
     def getActors(self):
@@ -106,4 +108,4 @@ if __name__ == '__main__':
     game.appendActor(actor3)
     game.appendActor(actor4)
     game.appendActor(actor5)
-    game.play(10)
+    game.play()
